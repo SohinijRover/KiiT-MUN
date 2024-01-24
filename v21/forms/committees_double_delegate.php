246e56eb58929a9efa7968dd2cf032a73f740fc1
Sohini Joarder <sohini.joarder@gmail.com>
2024-01-24 10:35:34 +0530

    v21

---
+++ v21/forms/committees_double_delegate.php
@@ -0,0 +1,7 @@
+<option>World Health Assembly (WHA)</option>
+<option>United nations Human Rights Council (UNHRC)</option>
+<option>Intergovernmental Panel on Climate Change (IPCC)</option>
+<option>World Bank</option>
+<option>United Nations Conference on Trade and Development</option>
+
+
